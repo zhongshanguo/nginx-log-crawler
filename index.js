8c8622e5d86654e1db86a23b1b21732baf83442d
lls <lls@gddata.net>
2020-02-16 12:06:38 +0800

    支持启动参数

--- index.js
+++ index.js
@@ -5,18 +5,56 @@ var fs = require("fs");
 var readline = require('readline');
 var http = require('http');
 
+let server = '127.0.0.1';
+
+function send(log) {
+    var str = JSON.stringify(log);
+    console.log(str);
+    var options = {
+        hostname: server,
+        port: 39999,
+        path: '/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        }
+    };
+    var req = http.request(options,
+        function (res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                JSON.parse(chunk)
+            });
+        });
+    req.on('error', function (e) {
+        console.log('problem with request: ' + e.message);
+    });
+    req.write(str);
+    req.end();
+}
+
 function readLog(rl) {
     rl.on("line", function (line) {
-        console.log(' ');
-        console.log(line);
-        console.log(' ');
+        send({log: line});
     });
 }
 
 function main() {
+    let serverArgPos = 0;
+    serverArgPos = process.argv.indexOf('-s');
+    if (serverArgPos < 1) {
+        serverArgPos = process.argv.indexOf('s');
+    }
+    if (serverArgPos > 0) {
+        serverArgPos++;
+        if (process.argv.length > serverArgPos) {
+            server = process.argv[serverArgPos];
+        }
+    }
+
     var output = fs.createWriteStream("/dev/null");
     var rl = readline.createInterface(process.stdin, output);
     readLog(rl);
 }
 
-main();
+void main();
